20726d62b9b4cf6bf938a81547b323d9d17e16f9
Vinh Chung <Vinh.Chung@datacom.com.au>
2022-02-23 10:40:43 +1100

    custom deploy script

---
+++ CreateDeployContentPackage.ps1
@@ -0,0 +1,1 @@
+echo "Executing script at target on the server"
---
+++ deploy.ps1
@@ -0,0 +1,7 @@
+echo "Unzipping and copy files over"
+
+Expand-Archive -LiteralPath $Env:DEPLOYMENT_SOURCE\release.zip -DestinationPath $Env:DEPLOYMENT_TARGET
+
+#robocopy "$Env:DEPLOYMENT_SOURCE" "$Env:DEPLOYMENT_TARGET" /E /XF "$Env:DEPLOYMENT_SOURCE\.deployment" "$Env:DEPLOYMENT_SOURCE\deploy.ps1"
+
+powershell "$Env:DEPLOYMENT_TARGET\CreateDeployContentPackage.ps1"
