8a91edf5244a75d0e6758775a89cd68e2dc63e36
Vinh Chung <Vinh.Chung@datacom.com.au>
2022-02-23 13:57:51 +1100

    custom deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,6 @@ echo "--Unzipping and copy files over"
 
 #Expand-Archive -LiteralPath $Env:DEPLOYMENT_SOURCE\release.zip -DestinationPath $Env:DEPLOYMENT_SOURCE\_build
 
-robocopy "$Env:DEPLOYMENT_SOURCE" "$Env:DEPLOYMENT_TARGET" /E /XF "$Env:DEPLOYMENT_SOURCE\.deployment" "$Env:DEPLOYMENT_SOURCE\deploy.ps1" | echo
+robocopy "$Env:DEPLOYMENT_SOURCE" "$Env:DEPLOYMENT_TARGET" /E /XF "$Env:DEPLOYMENT_SOURCE\.deployment" "$Env:DEPLOYMENT_SOURCE\deploy.ps1"
 
-powershell "$Env:DEPLOYMENT_TARGET\CreateDeployContentPackage.ps1" | echo
+powershell "$Env:DEPLOYMENT_TARGET\CreateDeployContentPackage.ps1"
